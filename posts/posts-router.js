80ab1cbcc117a200ab187eb24cdae561a41dc27b
Alan Perez <alanp9six@gmail.com>
2019-04-23 15:17:12 -0500

    mvp completed

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -39,48 +39,60 @@ router.get('/', async (req, res) => {
   });
   
   router.post('/', async (req, res) => {
-    try {
-      const posts = await Hubs.add(req.body);
-      res.status(201).json(hub);
-    } catch (error) {
-      // log error to database
-      console.log(error);
-      res.status(500).json({
-        message: 'Error adding the hub',
+    const {
+      title,
+      contents
+    } = req.body;
+    if (title && contents) {
+      try {
+        const post = await db.insert({
+          title,
+          contents
+        });
+        res.status(201).json(post);
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        })
+      }
+    } else {
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
       });
     }
   });
   
   router.delete('/:id', async (req, res) => {
     try {
-      const posts = await Posts.remove(req.params.id);
-      if (posts > 0) {
-        res.status(200).json({ message: 'The hub has been nuked' });
+      const post = await db.remove(req.params.id);
+      if (post > 0) {
+        res.status(200).json({ message: "post deleted" });
       } else {
-        res.status(404).json({ message: 'The hub could not be found' });
+        res.status(404).json({ message: "post dont exist" });
       }
     } catch (error) {
       // log error to database
       console.log(error);
       res.status(500).json({
-        message: 'Error removing the hub',
+        message: 'Error removing the post',
       });
     }
   });
   
   router.put('/:id', async (req, res) => {
     try {
-      const possts = await Hubs.update(req.params.id, req.body);
-      if (posts) {
-        res.status(200).json(posts);
+      const post = await db.update(req.params.id, req.body);
+      if (post) {
+        res.status(200).json(post);
       } else {
-        res.status(404).json({ message: 'The hub could not be found' });
+        res.status(404).json({ message: 'The post could not be found' });
       }
     } catch (error) {
       // log error to database
       console.log(error);
       res.status(500).json({
-        message: 'Error updating the hub',
+        message: 'Error updating the post',
       });
     }
   });
